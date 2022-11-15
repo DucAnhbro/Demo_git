f1f2a310dfbb0722c29b580e4b94854f286ca1ca
DucAnhbro <luuanhql456123@gmail.com>
2022-11-15 10:03:50 +0700

    demo second program

---
+++ Student/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Read();
+            Console.WriteLine("Tên tôi là:" + Console.ReadLine()); ;
+            Console.ReadKey();
+        }
+    }
+}
